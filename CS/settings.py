1b82aef70491f12a0beaae1255c157380bb12883
Eduardo Díaz <183442@ids.upchiapas.edu.mx>
2020-01-27 17:43:05 -0600

    migations

--- CS/settings.py
+++ CS/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(_m(4x^0c+zlumh-*uzsk9v$&%)g21rweq%w3qv5tzte-4ks7o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','35.174.3.78',] #'localhost', 'ip', 'dominio' solo numeros
 
